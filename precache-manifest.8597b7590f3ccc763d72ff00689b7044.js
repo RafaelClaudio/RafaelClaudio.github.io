2bf3ae0bf5fcb7bfc329804c97d3018cca3ce5c1
“RafaelClaudio” <raphael.phizaro123@gmail.com>
2019-11-21 14:54:04 +0700

    first commit

---
+++ precache-manifest.8597b7590f3ccc763d72ff00689b7044.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff5a8828fd42efa0cc6",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "648bdb63e38f0258db67",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4b895ef51e735377d9691238eb563d77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff5a8828fd42efa0cc6",
+    "url": "/js/about.1e13f265.js"
+  },
+  {
+    "revision": "3c7adcd99f8f9e18afef",
+    "url": "/js/app.c89f014e.js"
+  },
+  {
+    "revision": "648bdb63e38f0258db67",
+    "url": "/js/chunk-vendors.f89ce09a.js"
+  },
+  {
+    "revision": "3bddfb7501a3c16526913e3d3ee1e450",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
